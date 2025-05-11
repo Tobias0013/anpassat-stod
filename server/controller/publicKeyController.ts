33290117f29aef84d4e3c67c64ffe7b63f0fd3f5
Anton Bryhagen <anton.bryhagen@gmail.com>
2025-05-11 19:33:00 +0200

    Remove newline characters returned in pubkey

--- server/controller/publicKeyController.ts
+++ server/controller/publicKeyController.ts
@@ -5,7 +5,7 @@ import { join } from "path";
 export const getPublicKey = async (req: Request, res: Response) => {
     try{
         const publicKeyPath = join(__dirname, "..", "..", "server", "keys", "public.pem");
-        const publicKey = readFileSync(publicKeyPath, "utf-8");
+        const publicKey = readFileSync(publicKeyPath, "utf-8").replace(/\n/g, "");
     
         res.status(200).json({ publicKey });
     }catch(err){
